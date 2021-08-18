51d8b11a01c162e05d11ca0367a38f9e5a4d7762
ahsouza <annibalhsouza@gmail.com>
2021-08-18 19:58:01 -0300

    chore:new  add custom attribute that allows only a valid range for the Year property

---
+++ Infrastructure/CustomAttributes/YearRangeAttribute.cs
@@ -0,0 +1,11 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Infrastructure.CustomAttributes
+{
+  public class YearRangeAttribute : RangeAttribute
+  {
+    public YearRangeAttribute() : base(typeof(int), DateTime.Now.AddYears(-50).Year.ToString(), DateTime.Now.AddYears(1).Year.ToString())
+    { }
+  }
+}
